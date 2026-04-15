7b0dfa713671dcaeeb2a389f27bdec5298ac22c8
Craig Gidney <craiggidney@google.com>
2026-04-14 20:38:33 -0700

    Also fix HMR bit write being unconditional

--- lib/src/sim.rs
+++ lib/src/sim.rs
@@ -142,7 +142,8 @@ impl<'a, R: sha3::digest::XofReader> Simulator<'a, R> {
                     self.xof.read(&mut buf);
                     let rng_val = u64::from_le_bytes(buf);
                     let r = rng_val & cond;
-                    *self.bit_mut(op.c_target) = r;
+                    *self.bit_mut(op.c_target) &= !cond;
+                    *self.bit_mut(op.c_target) ^= r;
                     let v = self.qubit(op.q_target) & self.bit(op.c_target);
                     self.phase ^= v;
                     *self.qubit_mut(op.q_target) &= !cond;
--- lib/tests/test_sim.rs
+++ lib/tests/test_sim.rs
@@ -70,6 +70,29 @@ fn test_conditional_hmr() {
 }
 
 #[test]
+fn test_conditional_hmr_bit1() {
+    let circuit = Circuit::from_text("
+        BIT_STORE0 b0
+        BIT_STORE1 b1
+        X q0
+        HMR q0 b1 if b0
+        X q0
+    ");
+    let hasher = Shake256::default();
+    let mut xof = hasher.finalize_xof();
+    let mut sim = Simulator::new(
+        circuit.num_qubits as usize,
+        circuit.num_bits as usize,
+        &mut xof,
+    );
+    sim.clear_for_shot();
+    sim.apply_iter(circuit.operations.iter());
+    assert_eq!(sim.qubit(QubitId(0)), 0);
+    assert_eq!(sim.bit(BitId(0)), 0);
+    assert_eq!(sim.bit(BitId(1)), !0);
+}
+
+#[test]
 fn test_push_conditional_hmr() {
     let circuit = Circuit::from_text("
         BIT_STORE0 b0
